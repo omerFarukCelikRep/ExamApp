5d59366c62fa6c48d98179d41d26dcfe7f712fe2
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-14 18:20:48 +0300

    Error handling refactor

--- src/WebApi/ExamApp.Api.WebApi/Controllers/ApiController.cs
+++ src/WebApi/ExamApp.Api.WebApi/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using ErrorOr;
 using ExamApp.Api.WebApi.Common.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ExamApp.Api.WebApi.Controllers;
 
@@ -9,16 +10,39 @@ public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
-        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
-        var firstError = errors[0];
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
 
-        var statusCode = firstError.Type switch
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             ErrorType.NotFound => StatusCodes.Status404NotFound,
             _ => StatusCodes.Status500InternalServerError
         };
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        errors.ForEach(error => modelStateDictionary.AddModelError(error.Code, error.Description));
+
+        return ValidationProblem();
     }
 }
