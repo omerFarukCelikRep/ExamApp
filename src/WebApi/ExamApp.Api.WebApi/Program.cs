170ac88bc18dd01671b4befa509caf8eb4c51175
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-25 00:37:28 +0300

    Authentication and Authorization called

--- src/WebApi/ExamApp.Api.WebApi/Program.cs
+++ src/WebApi/ExamApp.Api.WebApi/Program.cs
@@ -14,6 +14,10 @@ var app = builder.Build();
 app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
